724ed3469aaa44bfd97d6a7be2f1ef8346b36bfe
Satabhisha <satabhisha.innits@gmail.com>
2020-08-24 13:00:43 +0530

    added Flag_activity_clear_task for logout

--- Payroll/app/src/main/java/org/wrkplan/payroll/Home/HomeActivity.java
+++ Payroll/app/src/main/java/org/wrkplan/payroll/Home/HomeActivity.java
@@ -199,6 +199,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 //                                a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
                                 Intent intent=new Intent(HomeActivity.this,LoginActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                 startActivity(intent);
                                 finish();
 
