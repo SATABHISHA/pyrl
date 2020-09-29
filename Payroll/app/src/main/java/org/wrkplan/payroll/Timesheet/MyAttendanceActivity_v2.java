50fc6abcab831d7b7f333817edc29a543ca6d4dd
Satabhisha Roy <satabhisha.innits@gmail.com>
2020-09-29 15:51:11 +0530

    working on timesheet

---
+++ Payroll/app/src/main/java/org/wrkplan/payroll/Timesheet/MyAttendanceActivity_v2.java
@@ -0,0 +1,16 @@
+package org.wrkplan.payroll.Timesheet;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.wrkplan.payroll.R;
+
+public class MyAttendanceActivity_v2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_attendance_v2);
+    }
+}
