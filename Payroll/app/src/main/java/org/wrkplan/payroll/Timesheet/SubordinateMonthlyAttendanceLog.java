40dc62ff19b31dbad1f3f7ceb91c45e9b0ef50ec
Satabhisha Roy <satabhisha.innits@gmail.com>
2020-11-03 17:51:45 +0530

    modified

---
+++ Payroll/app/src/main/java/org/wrkplan/payroll/Timesheet/SubordinateMonthlyAttendanceLog.java
@@ -0,0 +1,16 @@
+package org.wrkplan.payroll.Timesheet;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.wrkplan.payroll.R;
+
+public class SubordinateMonthlyAttendanceLog extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subordinate_monthly_attendancelog);
+    }
+}
