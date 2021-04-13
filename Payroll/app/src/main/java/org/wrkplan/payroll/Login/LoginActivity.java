f5b916f02ce86cdf47137d5318100d73b621c80b
Satabhisha Roy <satabhisha.innits@gmail.com>
2021-04-13 12:53:47 +0530

    modified

--- Payroll/app/src/main/java/org/wrkplan/payroll/Login/LoginActivity.java
+++ Payroll/app/src/main/java/org/wrkplan/payroll/Login/LoginActivity.java
@@ -67,7 +67,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 //    final  String url="http://192.168.10.175:9018/api/login/payroll_713/1/1";
 
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
