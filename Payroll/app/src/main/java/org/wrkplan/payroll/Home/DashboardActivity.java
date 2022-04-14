94175752e7639a7839d81fa761f402ac58e7c154
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-04-14 11:02:11 +0530

    modified

---
+++ Payroll/app/src/main/java/org/wrkplan/payroll/Home/DashboardActivity.java
@@ -0,0 +1,16 @@
+package org.wrkplan.payroll.Home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.wrkplan.payroll.R;
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
