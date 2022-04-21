8ca17496af37c0ccaf537f0fab1f17b10affc73f
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-04-21 19:17:16 +0530

    modified

---
+++ Payroll/app/src/main/java/org/wrkplan/payroll/Notifications/NotificationActivity.java
@@ -0,0 +1,16 @@
+package org.wrkplan.payroll.Notifications;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.wrkplan.payroll.R;
+
+public class NotificationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
