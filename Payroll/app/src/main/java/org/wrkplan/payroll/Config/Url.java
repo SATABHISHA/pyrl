3d82f176c6a1247a770f39d0f35b26a5029ccce2
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-06-01 14:43:12 +0530

    modified

--- Payroll/app/src/main/java/org/wrkplan/payroll/Config/Url.java
+++ Payroll/app/src/main/java/org/wrkplan/payroll/Config/Url.java
@@ -34,11 +34,10 @@ public static boolean isMyLeaveApplication=false;
   public static String BASEURL(){
     String url = "";
     if(LoginActivity.url_check.contentEquals("test")){
-//      url = "http://220.225.40.151:9018/api/";
-//      url = "http://203.163.248.154:9018/api/";
       url = "http://14.99.211.60:9018/api/";
     }else if(LoginActivity.url_check.contentEquals("live")){
-      url = "https://arb-erp.com/mergepr/mobile/api/";
+//      url = "https://arb-erp.com/mergepr/mobile/api/";
+      url = "https://arb-erp.com/payroll/mobile/api/";
     }
 
     return url;
